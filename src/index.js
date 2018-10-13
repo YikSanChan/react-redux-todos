51155162d66e08a28da7634133e4bb002933b9cf
Evan Chan <evan.chanyiksan@gmail.com>
2018-10-13 16:50:57 -0700

    Reducer composition with combineReducers()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 const todo = (state, action) => {
   switch (action.type) {
     case "ADD_TODO":
@@ -39,9 +41,7 @@ const visibilityFilter = (state = "SHOW_ALL", action) => {
   }
 };
 
-const todoApp = (state = {}, action) => {
-  return {
-    todos: todos(state.todos, action),
-    visibilityFilter: visibilityFilter(state.visibilityFilter, action)
-  };
-};
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+});
