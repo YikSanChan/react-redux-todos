0b9c17dec3e34c61f4899fda2e51a10d6067417d
Evan Chan <evan.chanyiksan@gmail.com>
2018-10-14 01:09:51 -0700

    Modularize the single file project

---
+++ src/components/AddTodo.js
@@ -0,0 +1,26 @@
+import { connect } from "react-redux";
+import { addTodo } from "../actions";
+import React from "react";
+
+const AddTodo = ({ dispatch }) => {
+  let input;
+  return (
+    <div>
+      <input
+        ref={node => {
+          input = node;
+        }}
+      />
+      <button
+        onClick={() => {
+          dispatch(addTodo(input.value));
+          input.value = "";
+        }}
+      >
+        Add Todo
+      </button>
+    </div>
+  );
+};
+
+export default connect()(AddTodo);
