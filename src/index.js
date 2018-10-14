0b18119e3a3da9b1ed5024611a3944d8ac8584f8
Evan Chan <evan.chanyiksan@gmail.com>
2018-10-14 09:33:17 -0700

    Supply the initial store state

--- src/index.js
+++ src/index.js
@@ -5,8 +5,18 @@ import { Provider } from "react-redux";
 import todoApp from "./reducers";
 import App from "./components/App";
 
+const persistedState = {
+  todos: [
+    {
+      id: 0,
+      text: "Welcome Back!",
+      completed: false
+    }
+  ]
+};
+
 ReactDOM.render(
-  <Provider store={createStore(todoApp)}>
+  <Provider store={createStore(todoApp, persistedState)}>
     <App />
   </Provider>,
   document.getElementById("root")
